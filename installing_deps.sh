4530c0148129266e483ccfa7c0cd01bd6bbd5dba
Jānis Džeriņš <janis.dzerins@cert.lv>
2017-02-03 16:57:12 +0200

    Fix libfuzzy-dev install command

--- installing_deps.sh
+++ installing_deps.sh
@@ -23,7 +23,7 @@ sudo apt-get install graphviz -y
 #needed for mathplotlib
 sudo easy_install -U distribute
 # ssdeep
-sudo apt-get install libfuzzy-dev
+sudo apt-get install libfuzzy-dev -y
 sudo apt-get install build-essential libffi-dev automake autoconf libtool -y
 
 # REDIS #
